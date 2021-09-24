ba4ba2e183784a9454e05c9b1e2605776bde557d
OrenLai <oren0602@gmail.com>
2021-09-25 07:16:20 +0800

    add management/delete page for paintings

--- app.js
+++ app.js
@@ -1,131 +1,129 @@
-require("dotenv").config();
+
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
-const mysql = require("mysql2");
 const { result } = require("lodash");
+const passport = require("passport-local");
+const multer = require("multer");
+const uuid = require("uuid");
+
+const database = require("./SqlConnection");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
-const con = mysql.createConnection({
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
-
-con.connect(function(err){
-    if (err){
-        console.log(err);
-    }else{
-        console.log("MYSQL connected !");
-        //create Database
-        // con.query("CREATE DATABASE OrenDB", function (err, result) {
-        //     if (err) throw err;
-        //     console.log("Database created");
-        //   });
-        
-
-        //create TABLE names paintings
-        // const sql1 = "CREATE TABLE paintings(id INT AUTO_INCREMENT PRIMARY KEY,name VARCHAR(255) ,description VARCHAR(255) , price INT)";
-        // con.query(sql1,function(err,result){
-        //     if(err){
-        //         console.log(err);
-        //     }else{
-        //         console.log("new table created");
-        //     }
-
-        // })
-        
-        // INSERT 1 record
-        // const sql = "INSERT INTO paintings (name,description,price) VALUES('paint1','watercolor',999)";
- 
-        // con.query(sql,function(err,result){
-        //     if(err){
-        //         console.log(err);
-        //     }else {
-        //         console.log("1 record inserted !");
-        //     };
-        // })
-
-
-        //INSERT multiple records
-        // const sql = "INSERT INTO paintings (name,description,price) VALUES ?"
-        
-        // const values = [
-        //     ['paint1', 'watercolor',111],
-        //     ['paint2', 'aqulics',222],
-        //     ['paint3', 'pancils',333],            
-        // ];
-        // con.query(sql,[values],function(err,result){
-        //     if(err){
-        //         console.log(err);
-        //     }else{
-        //         console.log("multiple records inserted into the table :",result.affectedRows);
-        //     }
-        // });
-
-        // different ways to SELECT  from the table
-        //const sql = "SELECT * FROM paintings ORDER by price DESC"; sorted by price ,desc order
-        //escape the query from user , use mysql.escape()
-        // const des = "watercolor";
-        // const prc = "777";
-        // const sql = "SELECT * FROM paintings WHERE description = ? AND price = ?";
-        
-        // select all result with price starts with 7 like 777 or 766 or 7xxxx
-        //const sql = "SELECT * FROM paintings WHERE price LIKE '7%'" 
-
-        //const sql = "DELETE from paintings WHERE description ='aqulics'";
-
-        //const sql = "ALTER TABLE paintings ADD imgsrc VARCHAR(255)"; 
-        // add a new column imgsrc into the table
-        
-        const sql = "UPDATE paintings SET name ='Wind and Dirt' WHERE name = 'paint3'"
-
-        con.query(sql, function(err, result){
-            if(err) 
-                console.log(err);
-            else 
-                //console.log(fields); the fields object is an array containing information about each field as an object
-                console.log(result);
-        })
+const storage = multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null, './public/images/arts');        
+    },
+    filename:function(req,file,cb){
+        //descructure from the file to get the oirginal file name
+        const {originalname} = file;
+        cb(null,originalname);
     }
 })
 
+// use multer to process the file upload , it will create a folder if the dest specified here doesn't exist 
+//const upload = multer({dest:'./public/images/'}) 
+const upload = multer({storage});
+
 app.set("view engine","ejs");
 
-app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 app.get("/",function(req,res){
-    con.query("SELECT * FROM paintings",function(err,result){
+    database.query("SELECT * FROM paintings",function(err,result){
     if(err) 
         console.log(err);
     else{
-        console.log(result);
+        //console.log(result);
         res.render("home",{
             paintings:result
         });
-    } 
-        
+    }         
     })
-
 });
 
 app.get("/login",function(req,res){
     res.render("login");
 });
 
-app.get("/favorites",function(req,res){
-    res.render("favorites");
+app.get("/management",function(req,res){
+
+    database.query("SELECT * FROM paintings",function(err,result){
+        if(err) 
+            console.log(err);
+        else{
+            //console.log(result);
+            res.render("management",{
+                paintings:result
+            });
+        }         
+        })
+});
+
+app.get("/delete",function(req,res){
+    database.query("SELECT * FROM paintings",function(err,result){
+    if(err) 
+        console.log(err);
+    else{
+        //console.log(result);
+        res.render("delete",{
+            paintings:result
+        });
+    }         
+    })
 });
 
 app.get("/shoppingcart",function(req,res){
     res.render("shoppingcart");
 });
 
+app.post('/upload', upload.single('paintingImage'), function (req, res) {
+    // req.file is the name of your file in the form above, here 'uploaded_file'
+    // req.body will hold the text fields, if there were any 
+    console.log("file" + req.file.originalname +"uploaded");
+    //console.log(req.file, req.body.name)
+    
+    //INSERT 1 record
+    const addone_sql = "INSERT INTO paintings (name,description,price,imgsrc) VALUES (?,?,?,?);";
+    
+    let newPaintName = req.body.name;
+    let newDescription = req.body.description;
+    let newPrice = req.body.Price;
+    let newImageSrc = "/images/arts/"+ req.file.originalname;
+
+    //console.log(addone_sql);
+    //const sql = "INSERT INTO paintings (name,description,price,imgsrc) VALUES('paint4','watercolor',999,'/images/arts/p4.jpg')";
+    database.query(addone_sql,[newPaintName,newDescription,newPrice,newImageSrc],function(err,result){
+        if(err){
+            console.log(err);
+        }else {
+            console.log("1 record inserted !");
+        };
+    })
+
+    res.redirect("/");
+ });
+
+app.post('/delete',function(req,res){
+    
+    console.log("going to delete painting with id :" + req.body.checkbox);
+    
+    delete_query = "DELETE from paintings WHERE id = " + req.body.checkbox;
+    
+    database.query(delete_query, function(err,result){
+        if (err)
+            console.log(err);
+        else {
+            console.log("paint with id " + req.body.checkbox + " removed");
+        }
+    })
+
+    res.redirect("/delete");
+});
 
 app.listen(3000,function(){
     console.log("server is up and running at port 3000");
